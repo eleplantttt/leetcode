3c08c117d17c7baab4614ce5164c228af50de0eb
cxb <1206296077@qq.com>
2023-05-19 14:47:41 +0800

    单词拆分

---
+++ 代码随想录/动态规划/第六周/单词拆分.cpp
@@ -0,0 +1,22 @@
+//
+// Created by admin on 2023/5/19.
+//
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+bool wordBreak(string s, vector<string> &wordDict) {
+    unordered_set<string> wordSet(wordDict.begin(),wordDict.end());
+    vector<bool> dp(s.size() + 1, false);
+    dp[0] = true;
+    for (int i = 1; i <= s.size(); i++) {
+        for(int j = 0; j < i; j++){
+            string word = s.substr(j, i-j);
+            if(wordSet.find(word) != wordSet.end() && dp[j]){
+                dp[i] = true;
+            }
+        }
+    }
+    return dp[s.size()];
+}
