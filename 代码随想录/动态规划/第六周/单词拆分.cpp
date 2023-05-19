3c4a1b3aabb80fe3b537e128a15f12e47edaf58e
cxb <1206296077@qq.com>
2023-05-19 14:55:59 +0800

    单词拆分注释

--- 代码随想录/动态规划/第六周/单词拆分.cpp
+++ 代码随想录/动态规划/第六周/单词拆分.cpp
@@ -1,6 +1,10 @@
 //
 // Created by admin on 2023/5/19.
-//
+//重新开一个哈希表，保存字典内字符串
+//初始化dp为 false，大小为 s.size()+1
+//dp[0] = true    为了递归
+//截取j到i之间的字符串去哈希表中查找有无  && dp[j]
+
 #include <iostream>
 #include <vector>
 #include <unordered_set>
