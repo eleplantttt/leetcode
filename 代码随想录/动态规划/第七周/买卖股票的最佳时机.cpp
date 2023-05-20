baa0aa91b110a56b0ed2888747ee089095e315ca
cxb <1206296077@qq.com>
2023-05-20 13:11:44 +0800

    买卖股票的最佳时机Ⅱ

--- 代码随想录/动态规划/第七周/买卖股票的最佳时机.cpp
+++ 代码随想录/动态规划/第七周/买卖股票的最佳时机.cpp
@@ -1,6 +1,16 @@
 //
 // Created by admin on 2023/5/20.
 //动态规划
+//- dp[i][0] 表示第i天持有股票所得现金。
+//- dp[i][1] 表示第i天不持有股票所得最多现金
+
+//- dp[i][0] 表示第i天持有股票所得现金。
+//- dp[i][1] 表示第i天不持有股票所得最多现金
+//        第i天持有股票即dp[i][1]
+//- 不同点在于可以多次买卖，则持有股票的状态可以由昨天不持有而今天买入dp[i - 1][1]-prices[i]和昨天就持有推导dp[i - 1][0]
+//再来看看如果第i天不持有股票即dp[i][1]的情况， 依然可以由两个状态推出来
+//- 第i-1天就不持有股票，那么就保持现状，所得现金就是昨天不持有股票的所得现金 即：dp[i - 1][1]
+//- 第i天卖出股票，所得现金就是按照今天股票价格卖出后所得现金即：prices[i] + dp[i - 1][0]
 int maxProfit(vector<int>& prices) {
     vector<vector<int>> dp(2, vector<int>(2));
     dp[0][0] = -prices[0];
@@ -11,7 +21,6 @@ int maxProfit(vector<int>& prices) {
     }
     return dp[(prices.size()-1) % 2][1];
 }
-
 
 //贪心
 class Solution {
