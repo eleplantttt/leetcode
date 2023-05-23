007d9d1a8e642f425e5aa72c5fcda66dcfd0b366
cxb <1206296077@qq.com>
2023-05-23 10:44:57 +0800

    最长公共子序列

---
+++ 代码随想录/动态规划/第八周/最长公共子序列.cpp
@@ -0,0 +1,16 @@
+//
+// Created by admin on 2023/5/23.
+//不同点在于不满足相等条件的时候，$$dp[i][j] = max(dp[i-1][j],dp[i][j-1])$$
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> dp(text1.size()+1,vector<int>(text2.size()+1, 0));
+        for(int i = 1; i <= text1.size(); i++){
+            for(int j = 1; j <= text2.size(); j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[text1.size()][text2.size()];
+    }
